ea17f21b586ed176685decab59d8a0975fa8c1a6
maosan132 <maosan132@gmail.com>
2022-04-06 09:33:15 -0500

    Implement basic authentiaction

--- src/index.js
+++ src/index.js
@@ -1,21 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-class Header extends React.Component {
-  render () {
-    return (
-      <div>
-        <h1>Hello old World</h1>
-      </div>
-    );
-  }
-}
+// class Header extends React.Component {
+//   render () {
+//     return (
+//       <div>
+//         <h1>Hello old World</h1>
+//       </div>
+//     );
+//   }
+// }
 
 // function Header() {
 //   return <h1>Hello everybody!</h1>
 // }
 
-// const Header = () => <h1>Hello all!</h1>
+const Header = (props) => <h1>Hello {props.username} {props.lastName} at {year}</h1>
+
+const Layout = (props) => <div style={{backgroundColor: 'aquamarine'}}>{props.children}</div>
+
+const Login = () => <div><p>Please Login first</p><button>Login</button></div>
+
+const SignOUt = () => <button>Exit</button>
 
 const greeting = (
   <div>
@@ -29,19 +35,37 @@ const anotherGreeting = <div>  bye guys! {year}</div>;
 const rootNode = document.getElementById('root');
 const isReactUser = true;
 
+const isAuthenticated = true;
+
 // if (isReactUser) {
 //     ReactDOM.render(greeting, rootNode);
 // } else {
 //     ReactDOM.render(anotherGreeting, rootNode);
 // }
 
-function greet() {
-  if (isReactUser) {
-    return greeting;
-  } else {
-    return anotherGreeting;
-  }
-}
+// function greet() {
+//   if (isReactUser) {
+//     return greeting;
+//   } else {
+//     return anotherGreeting;
+//   }
+// }
 
 // ReactDOM.render(greet(), rootNode);
-ReactDOM.render(<Header />, rootNode);
+
+ReactDOM.render(
+  <Layout >
+    {/* {isAuthenticated ? <Header username="React" lastName="User" /> : <Login />}
+    {isAuthenticated && <SignOUt />} short circuiting operation */}
+    {isAuthenticated ? (
+      <>
+        <Header username="React" lastName="User" />
+        <SignOUt />
+      </>
+    ) :
+    <Login />
+    }
+    <footer>Copyright 2022</footer>
+  </Layout>,
+  rootNode
+);
