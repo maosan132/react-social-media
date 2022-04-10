855b5bb813f7d87c392837c60ed2be1b33ad9488
maosan132 <maosan132@gmail.com>
2022-04-09 22:35:28 -0500

    Clean a bit index

--- src/index.js
+++ src/index.js
@@ -1,22 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const baseUrl = 'https://api.github.com/users/'
-
-const year = new Date().getFullYear();
 const rootNode = document.getElementById('root');
 
 
 ReactDOM.render(
   < App />, rootNode
 );
-
-// const NewPage = () => {
-//   return <Layout>
-//     <Header username="New" lastName="Page" />
-//    <div>  New page here </div>
-//    <br />
-//   </Layout>
-// };
-
-// setTimeout(() => ReactDOM.render(<NewPage/>, rootNode), 2000);
