4a8df421ab07903fe26c9a569998f0cf8cb77ffb
maosan132 <maosan132@gmail.com>
2022-04-11 10:04:49 -0500

    Add content to createpost comp and uplift state to App, add the post list to a post variable, and show it to App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Login from './components/login';
 import Header from './components/Header';
+import CreatePost from './components/CreatePost';
 
 // APP HERE
 
 const App = () => {
-  const [user, setUser] = React.useState('dddd');
+  const [user, setUser] = React.useState('');
+  const [posts, setPosts] = React.useState([]);
+
+  useEffect(() => {
+    document.title = user ? `Welcome, ${user}!` : 'React User';
+  }, [user]);
 
   if (!user) {
     return <Login setUser={setUser}/>;
@@ -14,6 +20,18 @@ const App = () => {
   return (
     <>
       <Header user={user} setUser={setUser}/>
+      <CreatePost user={user} setPosts={setPosts} posts={posts}/>
+      {posts.map((post, i) => (
+        <div style={{border: 'solid 1px black', padding: 10, marginTop: 10}}>
+        {post.image && <img
+          src={URL.createObjectURL(post.image)}
+          alt={`Post ${i}`}
+          style={{height: 200, width: '100%', objectFit: 'cover' }}
+        />}
+          <p>{post.content.title}</p>
+          <p>{post.content.body}</p>
+        </div>
+      ))}
     </>
   );
 }
--- src/components/CreatePost.js
+++ src/components/CreatePost.js
@@ -1,9 +1,50 @@
 import React from 'react';
 
-const CreatePost = () => {
+const CreatePost = ({user, setPosts, posts}) => {
+  const [content, setContent] = React.useState({
+    title: '',
+    body: ''
+  });
+  const [image, setImage] = React.useState(null);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const post = {content, image, user};
+    const newPosts = [post, ...posts];
+    setPosts(newPosts);
+  }
+
   return (
     <div>
-      <h1>Create Post</h1>
+      <h2>Create Post</h2>
+      <form onSubmit={handleSubmit}>
+        <label>Title: </label><br />
+        <input
+          type="text"
+          onChange={e => setContent({ ...content, title: e.target.value })}
+        /><br />
+        <label>Body: </label><br />
+        <textarea
+          placeholder='Add post content'
+          onChange={e => setContent({ ...content, body: e.target.value })}
+        /><br />
+        <label>Add image: </label><br />
+        <input
+        type="file"
+        onChange={e => setImage(e.target.files[0])}
+        /><br /><br />
+        <button type="submit">Submit post </button>
+      </form>
+
+      {/* <div style={{border: 'solid', minHeight: 100, padding: 10}}>
+        <p>{content.title}</p>
+        {image && <img
+          // src={image ? URL.createObjectURL(image) : ''} // works but it has a phantom image
+          src={URL.createObjectURL(image)}
+          alt="Post"
+          style={{height: 100, width: 200, objectFit: 'cover' }}
+        />}
+      </div>*/}
     </div>
   );
 }
