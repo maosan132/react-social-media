fdf92fd40b4e8839eec5eb1ea747ced82e0be72b
maosan132 <maosan132@gmail.com>
2022-04-12 08:31:35 -0500

    As handleAppPost is a function that will be recreated every time the app renders, Wrap it in a useCallback hook.!

--- src/App.js
+++ src/App.js
@@ -6,17 +6,29 @@ import PostList from './components/PostList';
 
 // APP HERE
 
+const fxCount = new Set();
+
 const App = () => {
-  const [user, setUser] = React.useState('');
+  const [user, setUser] = React.useState('xxx');
   const [posts, setPosts] = React.useState([]);
+  const [count, setCount] = React.useState(0);
 
   useEffect(() => {
     document.title = user ? `Welcome, ${user}!` : 'React User';
   }, [user]);
 
-  const handleAddPost = newPost => {
-    setPosts([newPost, ...posts])
-  };
+  // const handleAddPost = newPost => {
+  //   setPosts([newPost, ...posts])
+  // };
+
+  const handleAddPost = React.useCallback(
+    newPost => {
+      setPosts([newPost, ...posts]);
+    }, [posts]
+  );
+
+  fxCount.add(handleAddPost);
+  console.log(fxCount);
 
   if (!user) {
     return <Login setUser={setUser}/>;
@@ -27,6 +39,7 @@ const App = () => {
       <Header user={user} setUser={setUser}/>
       <CreatePost user={user} handleAddPost={handleAddPost}/>
       {posts.length > 0 && <PostList posts={posts}/>}
+      <button onClick={() => setCount(prev => prev + 1)}>{count} +</button>
     </>
   );
 }
